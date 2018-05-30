21f26385d26d8399581b2120447550de16a6dbf4
Ilya Viatkin <iviatkin@integrotechnologies.ru>
2018-05-30 11:21:01 +0300

    add filter to admin

--- view/src/main/java/webbey/auth/AdminLogonFilter.java
+++ view/src/main/java/webbey/auth/AdminLogonFilter.java
@@ -1,11 +1,18 @@
 package webbey.auth;
 
+import javax.inject.Inject;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter(urlPatterns = "/admin/*")
 public class AdminLogonFilter implements Filter {
+
+    @Inject
+    private AuthBean authBean;
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -16,6 +23,15 @@ public class AdminLogonFilter implements Filter {
                          ServletResponse servletResponse,
                          FilterChain filterChain) throws IOException, ServletException {
 
+        if (authBean.isLoggetIn()){ // если уже залогинен то передаем дальше по цепочке
+            filterChain.doFilter(servletRequest,servletResponse);
+            return;
+        }
+        // иначе идем на страницу логина
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        authBean.setRequestedPage(request.getRequestURI()); // сохраняем URL который запрашивали
+        response.sendRedirect(request.getContextPath()+"/login.xhtml");
     }
 
     @Override
