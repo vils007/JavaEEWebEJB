6462af7cf79883e22ce31c55562b813b20487a75
Ilya Viatkin <iviatkin@integrotechnologies.ru>
2018-05-30 13:41:47 +0300

    add addUser ( есть косяк с id ее нужно добавить в последновательность - сейчас все всремя подставляет 0 и больше одного не зарагистрируешь)

--- logic/src/main/java/webbey/auth/ejb/AuthentificationManager.java
+++ logic/src/main/java/webbey/auth/ejb/AuthentificationManager.java
@@ -22,13 +22,14 @@ public class AuthentificationManager {
         Credentials credentials = entityManager.find(Credentials.class, email);
         if (credentials == null) return false;
 
-       if (!password.equals(credentials.getPassword())) return false;
+        if (!password.equals(credentials.getPassword())) return false;
 
         ShopUser shopUser = credentials.getShopUser();
-        if (shopUser == null)return false;
+        if (shopUser == null) return false;
 
         return true;
     }
+
     public boolean loginAsAdmin(String email, String password) {
         if (StringUtils.isEmpty(email) || StringUtils.isEmpty(password)) return false; //проверка на null и на пустоту
 
@@ -38,9 +39,35 @@ public class AuthentificationManager {
         if (!password.equals(credentials.getPassword())) return false;
 
         Admin admin = credentials.getAdmin();
-        if (admin == null)return false;
+        if (admin == null) return false;
 
         return true;
     }
 
+    public boolean addUser(String email, String password){
+        return addUser(email,password,"test");
+    }
+
+
+    public boolean addUser(String email, String password, String name) { // стоит в отдельный класс перенести
+        try {
+           // entityManager.getTransaction().begin();
+            Credentials credentials = new Credentials();
+            credentials.setEmail(email);
+            credentials.setPassword(password);
+            ShopUser shopUser = new ShopUser();
+            shopUser.setName(name);
+            shopUser.setCredentials(credentials);
+            //shopUser.setCredentials(credentials);
+            credentials.setShopUser(shopUser);
+
+            entityManager.persist(credentials);
+            entityManager.persist(shopUser);
+        //    entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("addUser do ot worked");
+            return false;
+        }
+        return true;
+    }
 }
